4523194027172e051f4fa49d263f4d571055ffc9
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-30 00:50:58 -0300

    criando server graphql

--- src/server.js
+++ src/server.js
@@ -1,3 +1,11 @@
-import app from './app';
+import { GraphQLServer } from 'graphql-yoga';
+import path from 'path';
+import resolvers from './app/resolvers/resolvers';
+import './database';
 
-app.listen(3000);
+const server = new GraphQLServer({
+  typeDefs: path.resolve(__dirname, '.', 'app', 'schemas', 'schema.graphql'),
+  resolvers,
+});
+
+server.start();
