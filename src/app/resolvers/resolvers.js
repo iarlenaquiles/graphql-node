27e630e31cc5e026834dfac60674d7f8d1c219fd
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-30 01:53:52 -0300

    criando o delete e o update

--- src/app/resolvers/resolvers.js
+++ src/app/resolvers/resolvers.js
@@ -9,5 +9,8 @@ module.exports = {
   Mutation: {
     createUser: (_, { name, last_name, email }) =>
       User.create({ name, last_name, email }),
+    deleteUser: (_, { id }) => User.destroy({ where: { id } }),
+    updateUser: (_, { id, name, last_name, email }) =>
+      User.update({ name, last_name, email }, { where: { id } }),
   },
 };
